3a1915aa3fb14dd7cff9c728b59ab6c9813e7945
Yalei Li <yalei.li@gmail.com>
2019-11-18 22:24:10 -0500

    added promote message when executing the script

--- container/chromebook-docker-install.sh
+++ container/chromebook-docker-install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # First, you need to isntall some pre-requisites
+echo ">>>>>> Intalling pre-requisites..."
 sudo apt-get update
 sudo apt-get install \
 	apt-transport-https \
@@ -9,6 +10,7 @@ sudo apt-get install \
 	software-properties-common
 
 # The add the Docker repository
+echo ">>>>>> Adding docker repository..."
 curl -fsSL https://download.docker/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository \
 	"deb [arch=amd64] https://download.docker.com/linux/ubuntu \
@@ -16,9 +18,11 @@ sudo add-apt-repository \
 	stable"
 
 # Then install Docker:
+echo ">>>>>> Installing Docker..."
 sudo apt-get update
 sudo apt-get install docker-ce
 
 # Once installation is completed, run docker hello-world to test the installation
+echo ">>>>>> Testing docker installation..."
 sudo docker run hello-world
 
